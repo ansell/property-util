f17015232ec11d3c8b143796b0a2dfdadddc74f6
Peter Ansell <p_ansell@yahoo.com>
2013-01-22 15:02:55 +1000

    add package-info.java

---
+++ src/main/java/com/github/ansell/propertyutil/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Property Util to abstract over fetching properties from both system properties, property bundles,
+ * with default fall back values.
+ * 
+ * @author Peter Ansell p_ansell@yahoo.com
+ * 
+ */
+package com.github.ansell.propertyutil;
---
+++ src/test/java/com/github/ansell/propertyutil/test/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Tests for PropertyUtil
+ * 
+ * @author Peter Ansell p_ansell@yahoo.com
+ * 
+ */
+package com.github.ansell.propertyutil.test;
